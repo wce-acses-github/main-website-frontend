9b76255681e8b7edccbfa5d27450498ae5511efa
om7057 <kulkarniom7057@gmail.com>
2024-09-30 12:35:19 +0530

    Final changes to css

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import Navbar from './components/Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer/Footer'
-import './input.css'
+import '../src/index.css'
 
 function Layout() {
   return (
